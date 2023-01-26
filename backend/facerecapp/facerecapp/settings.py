70365ab3c06305c25ccbb4864a7eabc9311bd27c
Janajan Jeyabalan <75293182+JJ0304@users.noreply.github.com>
2023-01-26 01:14:25 -0500

    Updated static files

--- backend/facerecapp/facerecapp/settings.py
+++ backend/facerecapp/facerecapp/settings.py
@@ -116,3 +116,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'reactapp/build')
+]
